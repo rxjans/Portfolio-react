dbb75c819bca108ad25b19a3b8d452e3202ed9aa
Rajan Sharma <rajansharma@192.168.1.2>
2025-02-02 00:20:29 +0530

    Big fixes

--- src/components/About.js
+++ src/components/About.js
@@ -13,11 +13,11 @@ const About = () => {
   return(
   
   <div className='section' id='about' ref={ref}>
-    <div className='container mx-auto'> 
+    <div className='container mx-auto hidden md:block'> 
       <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
       {/* img */}
         <motion.div
-        variants={fadeIn('right', 0.3)}
+        variants={fadeIn('right', 0.1)}
         initial='hidden'
         whileInView={'show'}
         viewport={{once: false, amount: 0.3}}
@@ -25,7 +25,7 @@ const About = () => {
         </motion.div>
       {/* text */}
         <motion.div
-        variants={fadeIn('left', 0.3)}
+        variants={fadeIn('left', 0.1)}
         initial='hidden'
         whileInView={'show'}
         viewport={{once: false, amount: 0.3}}
@@ -76,7 +76,7 @@ const About = () => {
           <div className='flex gap-x-12 items-center'>
             <button onClick={(e)=>{
                 e.preventDefault();
-                window.location.href='https://www.linkedin.com/in/rajan-sharma-418364242/overlay/1635542966012/single-media-viewer/?profileId=ACoAADw1h1QBM7HGrnA3-gcKqsUkY_HLkT3UW7s';
+                window.location.href='https://drive.google.com/file/d/1_ggEPAn_K830NBgqWBQ7zgnetWxrnpnj/view?usp=drive_link';
                 }} 
                 className='btn btn-lg'>Resume</button>
             <a href='home' className='text-gradient btn-link'>
@@ -84,6 +84,81 @@ const About = () => {
             </a>
           </div>
         </motion.div>
+      </div>
+    </div>  
+
+    {/* for medium screen sizes */}
+    <div className='container mx-auto md:hidden'> 
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-20 lg:gap-y-0 h-screen'>
+      {/* img */}
+        <div
+        variants={fadeIn('right', 0.1)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.3}}
+        className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[640px] bg-about bg-contain bg-no-repeat h-[640px] mix-blend-color-dodge opacity-60 bg-top'>
+        </div>
+      {/* text */}
+        <div
+        variants={fadeIn('left', 0.1)}
+        initial='hidden'
+        whileInView={'show'}
+        viewport={{once: false, amount: 0.3}}
+        className='flex-1'>
+          <h2 className='h2 text-gradient'>About Me.</h2>
+          <h3 className='h3 mb-4 text-[22px] leading-[1.2]'>I am a Full Stack Web Developer with entry-level experience specializing in user interface design, API development, data
+            security, and continuous improvement. Adept at identifying opportunities to enhance the user experience and
+            develop new features to improve web application functionality.
+          </h3>
+          <p className='mb-6 text-[16px] leading-[1]'>As far as I can remember, I've always been curious about gaming and the new emerging technologies. I used to wonder how the cogs are working behind the scene to provide with such smooth immersive experience. This curiosity led me to discover a world full of programmers that I ended up admiring as I grew up. I had a dream of contributing to this community I've been following for so long. I discovered various variations of work the programmers would take upon(Backend, Frontend, Designing, Website Development).
+          
+          Gradually, I found the best ideal field that had me hooked. I started working on my Web Development skills whenever I had free time, I would create different projects to help me better understand the core concepts of Development. After a year of Frontend Development, I switched to MERN Stack Development(MongoDB, Express.js, React.js, Node.js)</p>
+          {/* stats */}
+          <div className='flex gap-x-6 lg:gap-x-8 mb-12'>
+            <div>
+              <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                {inView ?
+                <CountUp start={0} end={6} duration={4} />: null}
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Years of <br />
+                Learning
+              </div>
+            </div>
+            <div>
+              <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                {inView ?
+                <CountUp start={0} end={10} duration={4} />: null}
+                +
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Major <br />
+                Projects Completed
+              </div>
+            </div>
+            <div>
+              <div className='text-[40px] font-tertiary text-gradient mb-2'>
+                {inView ?
+                <CountUp start={0} end={15} duration={4} />: null}
+                +
+              </div>
+              <div className='font-primary text-sm tracking-[2px]'>
+                Minor <br />
+                Projects Completed
+              </div>
+            </div>
+          </div>
+          <div className='flex gap-x-12 items-center'>
+            <button onClick={(e)=>{
+                e.preventDefault();
+                window.location.href='https://drive.google.com/file/d/1_ggEPAn_K830NBgqWBQ7zgnetWxrnpnj/view?usp=drive_link';
+                }} 
+                className='btn btn-lg'>Resume</button>
+            <a href='home' className='text-gradient btn-link'>
+              Home Page
+            </a>
+          </div>
+        </div>
       </div>
     </div>  
   </div>
--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,7 +8,7 @@ import {fadeIn} from '../variants';
 
 const Banner = () => {
   return <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
-    <div className='container mx-auto'>
+    <div className='container mx-auto hidden md:block'>
       <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
           <div className='flex-1 text-center font-secondary lg:text-left'>
             <motion.h1
@@ -92,6 +92,94 @@ develop new features to improve web application functionality
            className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mix-blend-color-dodge'>
             <img src={Image}></img>
           </motion.div>
+      </div>
+    </div>
+
+    {/* for small screen size */}
+    <div className='container mx-auto md:hidden'>
+      <div className='flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12'>
+          <div className='flex-1 text-center font-secondary lg:text-left'>
+            <h1
+            variants={fadeIn('up', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}} 
+            className='text-[55px] lg:text-[50px] font-bold leading[1] xl:text-[90px]'>
+            RAJAN <span>SHARMA</span>
+            </h1>
+            <div 
+            variants={fadeIn('left', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}} 
+            className='mb-6 text-[36px] md:text-[48px] lg:text-[30px] xl:text-[50px] font-secondary font-semibold leading-[2]'>
+              <span className='mr-4 text-white'>I am a</span>
+              <TypeAnimation sequence={[
+                'Front-End Developer',
+                2000,
+                'Back-End Developer',
+                2000,
+                'UI/UX Designer',
+                2000
+              ]}
+              speed={50}
+              className='text-accent'
+              wrapper='span'
+              repeat={Infinity}
+              />
+
+            </div>
+            <p 
+            variants={fadeIn('up', 1.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}} 
+            className='mb-8 max-w-lg mx-auto lg:mx-0'>A Web Developer with entry-level experience specializing in user interface design, API development, data
+security, and continuous improvement. Adept at identifying opportunities to enhance the user experience and
+develop new features to improve web application functionality
+            </p>
+            <div 
+            variants={fadeIn('left', 2.5)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}} 
+            className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+              <button onClick={(e)=>{
+                e.preventDefault();
+                window.location.href='#contact';
+                }} 
+                className='btn btn-lg'>Contact Me</button>
+              <a href='#' onClick={(e)=>{
+                e.preventDefault();
+                window.location.href='#home';
+              }} className='text-gradient btn-link'>My Portfolio</a>
+            </div>
+          
+            <div 
+            variants={fadeIn('right', 3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}} 
+            className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:max-0'>
+              <a href='https://github.com/rxjans'>
+              <FaGithub />
+              </a>
+              <a href='https://www.instagram.com/_.rxjans/'>
+              <FaInstagram />
+              </a>
+              <a href='https://www.linkedin.com/in/rajan-sharma-418364242/'>
+              <FaLinkedin />
+              </a>
+            </div>
+          </div>      
+          <div
+            variants={fadeIn('down', 0.3)}
+            initial='hidden'
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}} 
+           className='hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px] mix-blend-color-dodge'>
+            <img src={Image}></img>
+          </div>
       </div>
     </div>
   </section>
